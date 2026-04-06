d17805f3debf4201809cfc9b21eb5caf6992b6c5
SaraBrisa <sarabgmariano24@gmail.com>
2026-04-06 10:10:33 -0300

    login atualizado

--- src/App/pages/auth/AuthCard.tsx
+++ src/App/pages/auth/AuthCard.tsx
@@ -7,11 +7,19 @@ import './AuthCard.css';
 const AuthCard: React.FC = () => {
   return (
     <div className="auth-card">
-      <img
-        src="/images/Register-Image.png"
-        alt="MenuPoint - Garfo, prato e faca ilustrados"
-        className="auth-card__image"
-      />
+
+      {/* esse div é o gradiente de fundo */}
+      <div className="auth-card__gradient">
+
+        {/* a ilustração centralizada dentro do gradiente */}
+        <img
+          src="/images/Logo-Grande.png"
+          alt="MenuPoint - Garfo, prato e faca ilustrados"
+          className="auth-card__image"
+        />
+
+      </div>
+
     </div>
   );
 };
